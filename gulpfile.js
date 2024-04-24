99344c12fa575b5e10b5ae6122ef3bd4a5e19c85
fersh00 <fc323897@gmail.com>
2024-04-24 10:06:48 -0700

    se crea tarea gulp css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
+const { src, dest, watch } = require('gulp')
 
+const sass = require('gulp-sass')(require('sass'))
 
-export function tarea(done) {
-    console.log('Mi primer tarea');
-    done();
-}
+// export function tarea(done) {
+//     console.log('Mi primer tarea');
+//     done();
+// }
 
 //exports.tarea = tarea;
+
+function css(done) {
+	src('src/scss/app.scss') //Identificar arch SASS
+		.pipe(sass()) //Compilar
+		.pipe(dest('build/css')) //Almacenarlo en dd
+
+	done()
+}
+
+function dev(done) {
+    watch('src/scss/app.scss', css)
+    done()
+}
+
+exports.css = css
+exports.dev = dev
